824f5d454b0054b373de74600aa4ce527792c8cc
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-09-01 20:41:18 -0300

    Feature: Requisite 04 - created function scrape_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 # Requisito 1
 from parsel import Selector
+from urllib.parse import urlparse
 import time
 import requests
 
@@ -48,11 +49,26 @@ def scrape_next_page_link(html_content):
     return None
 
 
-
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+
+    news_data = {
+        "url": selector.css('link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css('h1.entry-title::text').get().strip(),
+        "timestamp": selector.css('li.meta-date::text').get().strip(),
+        "writer": selector.css('span.author a::text').get(),
+        "reading_time": int(selector.css(
+            'li.meta-reading-time::text').re_first(r'\d+')
+        ),
+        "summary": "".join(selector.css(
+            ".entry-content > p:first-of-type *::text"
+        ).getall()).strip(),
+        "category": selector.css('a.category-style span.label::text').get()
+
+    }
+
+    return news_data
 
 
 # Requisito 5
